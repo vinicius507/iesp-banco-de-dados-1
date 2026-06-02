118d276c26e5cfed8ddde155762c8d50c6681db9
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2026-06-02 18:13:52 -0300

    fix: corrige constraints, nomes de colunas e tabelas

--- src/init.sql
+++ src/init.sql
@@ -5,31 +5,31 @@
 
 CREATE TABLE departamentos (
     codigo INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    nome VARCHAR(50)
+    nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE empregados (
     codigo INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    nome VARCHAR(100)
+    nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE gerentes (
     codigo INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    nome VARCHAR(100)
+    nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE projetos (
     codigo INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    nome VARCHAR(100),
+    nome VARCHAR(100) NOT NULL,
     data_inicio DATE,
     data_fim DATE,
-    codigo_departamento INT REFERENCES departamentos (codigo),
-    codigo_gerente INT REFERENCES gerentes (codigo)
+    codigo_departamento INT REFERENCES departamentos (codigo) NOT NULL,
+    codigo_gerente INT REFERENCES gerentes (codigo) NOT NULL
 );
 
 CREATE TABLE empregados_projetos (
-    codigo_empregado INT REFERENCES empregados (codigo),
-    codigo_projeto INT REFERENCES projetos (codigo),
+    codigo_empregado INT REFERENCES empregados (codigo) NOT NULL,
+    codigo_projeto INT REFERENCES projetos (codigo) NOT NULL,
     horas_trabalhadas NUMERIC(2) DEFAULT 0,
     PRIMARY KEY (codigo_empregado, codigo_projeto)
 );
